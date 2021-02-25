26d2ee672f80b0c995fa58d374da223ee1894211
Maxim B <maxim.belyaevskiy@shtormtech.ru>
2021-02-25 16:06:41 +0300

    Initial web

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,16 +33,7 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-});
-</script>
+<script src="./HelloWorld.ts" lang="ts" />
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
